1f45bd002f5da11ff4eb3bd4ad816d7e34a914a8
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2025-08-11 11:19:37 +0300

    refactor: Enhance office event handler and request schema to support additional query parameters for filtering and sorting

--- server/domains/offices/indexOfficeEventHandlerFn.ts
+++ server/domains/offices/indexOfficeEventHandlerFn.ts
@@ -8,11 +8,25 @@ import { RepositoryProvider } from '~~/server/services';
 import { OfficeDTOMapper } from './officeDTOMapper';
 
 export const IndexOfficeEventHandlerFn: EventHandlerFn<IndexOfficeRequest> = async ({ path, query }) => {
-  const haveWhereQueries = RequestInputHelper.haveWhereQueries<IndexOfficeRequest>(query, []);
-  const where: Prisma.OfficeWhereInput | undefined = { AND: haveWhereQueries ? [] : undefined };
+  const haveWhereQueries = RequestInputHelper.haveWhereQueries<IndexOfficeRequest>(query, [
+    'arr[equals]',
+    'price[max]',
+    'price[min]',
+    'type[equals]',
+  ]);
+  const where: Prisma.OfficeWhereInput = {
+    AND: haveWhereQueries
+      ? [
+          query['arr[equals]'] ? { arr: query['arr[equals]'] } : {},
+          query['price[max]'] ? { price: { lte: query['price[max]'] } } : {},
+          query['price[min]'] ? { price: { gte: query['price[min]'] } } : {},
+          query['type[equals]'] ? { type: query['type[equals]'] } : {},
+        ]
+      : undefined,
+  };
   const totalCount = await RepositoryProvider.officeRepository.count({ where });
   const pagination = new Pagination({ page: query.page, pageSize: query.pageSize, path, totalCount });
-  const offices = await RepositoryProvider.officeRepository.findMany({ orderBy: [{ createdAt: query['orderBy[createdAt]'] }], skip: pagination.offset, take: pagination.pageSize, where });
+  const offices = await RepositoryProvider.officeRepository.findMany({ orderBy: [{ createdAt: query['orderBy[price]'] }], skip: pagination.offset, take: pagination.pageSize, where });
 
   return {
     data: OfficeDTOMapper.toDTOs(offices),
