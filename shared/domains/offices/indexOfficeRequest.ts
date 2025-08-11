2ae5eac1d9eaf8d60df0b681f1bdd5a1be42d5a5
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2025-08-11 19:48:08 +0300

    refactor: Update IndexOfficeRequestQuerySchema to use z.coerce for number validation in office request queries

--- shared/domains/offices/indexOfficeRequest.ts
+++ shared/domains/offices/indexOfficeRequest.ts
@@ -11,8 +11,8 @@ import { z } from 'zod';
 export type IndexOfficeRequestData = { data: OfficeDTO[]; pagination: PaginationDTO };
 
 export const IndexOfficeRequestQuerySchema = z.object({
-  'arr[equals]': z.number().min(1).max(20),
-  'arr[in]': z.array(z.number().min(1).max(20)),
+  'arr[equals]': z.coerce.number().min(1).max(20),
+  'arr[in]': z.array(z.coerce.number().min(1).max(20)),
   'orderBy[price]': SortOrderSchema,
   'price[gte]': z.number(),
   'price[lte]': z.number(),
