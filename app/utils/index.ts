ff5dc5398fb106c3e3cdb0f67a307599fc1a620b
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2025-08-11 19:48:50 +0300

    feat: Add pagination utility to generate page sizes for improved data handling

--- app/utils/index.ts
+++ app/utils/index.ts
@@ -1,6 +1,7 @@
 export * from '../../shared';
 export * from './dates';
 export * from './forms';
+export * from './paginations';
 export * from './requests';
 export * from './responses';
 export * from './routes';
