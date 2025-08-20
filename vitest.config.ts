2bf5492a85224f0130d784eadfb5aea8b963b84b
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2025-08-20 09:56:36 +0300

    chore: Update Vitest configuration to exclude additional directories and files from coverage reports

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,30 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     coverage: {
+      exclude: [
+        // @see https://vitest.dev/config/#coverage-exclude
+        'coverage/**',
+        'dist/**',
+        '**/node_modules/**',
+        '**/[.]**',
+        'packages/*/test?(s)/**',
+        '**/*.d.ts',
+        '**/virtual:*',
+        '**/__x00__*',
+        '**/\x00*',
+        'cypress/**',
+        'test?(s)/**',
+        'test?(-*).?(c|m)[jt]s?(x)',
+        '**/*{.,-}{test,spec,bench,benchmark}?(-d).?(c|m)[jt]s?(x)',
+        '**/__tests__/**',
+        '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*',
+        '**/vitest.{workspace,projects}.[jt]s?(on)',
+        '**/.{eslint,mocha,prettier}rc.{?(c|m)js,yml}',
+
+        // custom
+        'scripts/**',
+      ],
+
       provider: 'v8',
     },
     projects: [
@@ -38,7 +62,7 @@ export default defineConfig({
       await defineVitestProject({
         test: {
           environment: 'nuxt',
-          include: ['test/app/*.{test,spec}.ts'],
+          include: ['test/app/**/*.{test,spec}.ts'],
           name: 'nuxt',
         },
       }),
