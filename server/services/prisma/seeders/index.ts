2435be917c6243b6a4e664fef191879a023bc9b1
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2025-08-20 08:03:59 +0300

    feat: Add user roles seeder and integrate audit log creation into the main seeder process

--- server/services/prisma/seeders/index.ts
+++ server/services/prisma/seeders/index.ts
@@ -1,8 +1,10 @@
 // We use nested imports to avoid interference with Nuxt ecosystem
 import { PrismaClient } from '../../../../generated/prisma/client';
+import { createAuditLogs } from './auditLogs';
 import { createLeads } from './leads';
 import { createOffices } from './offices';
 import { createServices } from './services';
+import { createUserRoles } from './userRoles';
 
 const prismaClient = new PrismaClient();
 
@@ -10,6 +12,16 @@ const main = async () => {
   const simulationYears: number = 2;
 
   console.time('=> Total seed duration');
+
+  /* --------------------------------- User Roles -------------------------------- */
+  console.time('User Roles seed duration');
+
+  const userRoles = await createUserRoles({
+    count: 5,
+    prisma: prismaClient,
+  });
+
+  console.timeEnd('User Roles seed duration');
 
   /* --------------------------------- Services -------------------------------- */
   console.time('Services seed duration');
@@ -35,13 +47,28 @@ const main = async () => {
   /* ---------------------------------- Leads ---------------------------------- */
   console.time('Leads seed duration');
 
-  await createLeads({
+  const leads = await createLeads({
     offices,
     prisma: prismaClient,
     years: simulationYears,
   });
 
   console.timeEnd('Leads seed duration');
+
+  /* --------------------------------- Audit Logs -------------------------------- */
+  console.time('Audit Logs seed duration');
+
+  await createAuditLogs({
+    auditLogsPerEntity: 3,
+    leads,
+    offices,
+    prisma: prismaClient,
+    services,
+    userRoles,
+    years: simulationYears,
+  });
+
+  console.timeEnd('Audit Logs seed duration');
 
   console.timeEnd('=> Total seed duration');
 };
